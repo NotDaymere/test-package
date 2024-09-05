b8d2372bf23ef73915e8991a14e4e66039bbe344
NotDaymere <ikonotopchick@gmail.com>
2024-09-06 00:35:38 +0300

    update

--- packages/utils/getCurrentFormatDate.ts
+++ packages/utils/getCurrentFormatDate.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { GetCurrentFormatDate } from "..";
+import { GetCurrentFormatDate } from "../index";
 
 export const getCurrentFormatDate: GetCurrentFormatDate = () => {
     return "Current Date: " + dayjs().format('YYYY-MM-DD / H:mm:s')
