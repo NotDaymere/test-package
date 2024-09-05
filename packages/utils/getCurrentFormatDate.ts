575d4c36205d91236dbe27bfa3676173271ea08e
NotDaymere <ikonotopchick@gmail.com>
2024-09-06 00:25:37 +0300

    update package

---
+++ packages/utils/getCurrentFormatDate.ts
@@ -0,0 +1,6 @@
+import dayjs from "dayjs";
+import { GetCurrentFormatDate } from "../..";
+
+export const getCurrentFormatDate: GetCurrentFormatDate = () => {
+    return "Current Date: " + dayjs().format('YYYY-MM-DD / H:mm:s')
+}
--- workspace/utils/getCurrentFormatDate.js
+++
@@ -1,5 +0,0 @@
-import dayjs from "dayjs";
-
-export function getCurrentFormatDate() {
-    return "Current Date: " + dayjs().format('YYYY-MM-DD / H:mm:s')
-}
